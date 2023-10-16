3042dd9a0de262612d656cfa9ab19f4425c1e788
hyeonsu <105619531+SSSSSSu3834@users.noreply.github.com>
2023-10-16 18:39:50 +0900

    글로벌 스타일

---
+++ styles/globalStyles.ts
@@ -0,0 +1,10 @@
+import { css } from "@emotion/react";
+
+export const GlobalStyles = css`
+  * {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+    font-family: Noto Sans CJK KR, Helvetica, sans-serif;
+  }
+`;
